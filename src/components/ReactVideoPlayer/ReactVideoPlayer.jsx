c93e7dc2c591bdf39959059f86da44076e185421
Veldas R Durai <veldasrdurai72@gmail.com>
2021-05-19 21:00:48 +0530

    created loading notation before video on ready

--- src/components/ReactVideoPlayer/ReactVideoPlayer.jsx
+++ src/components/ReactVideoPlayer/ReactVideoPlayer.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactPlayer from 'react-player';
 import styled from 'styled-components';
 import { useHistory } from 'react-router-dom';
@@ -12,21 +12,23 @@ const Div = styled.div`
 
 const ReactVideoPlayer = () => {
     const history = useHistory();
+    const [ loading, setLoading ] = useState(true);
     return (
         <Div>
             <div onClick={ () => history.push('/') } > Back </div>
+            {   loading && 'Loading...' }
             <ReactPlayer
                 url='https://youtu.be/47bXqs0Zs7E'
                 controls
                 height='250px' width='500px'
-                onReady={() => console.log('onReady callback')}
+                onReady={() => setLoading(false)}
                 onStart={() => console.log('onStart callback')}
                 onPause={() => console.log('onPause callback')}
                 onEnded={() => console.log('onEnded callback')}
                 onError={() => console.log('onError callback')}
             />
         </Div>
-  )
+    );
 }
 
 export default ReactVideoPlayer;
